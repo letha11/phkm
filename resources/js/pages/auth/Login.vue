84907671c87ceea849e6212c9cdad8cf926343d8
letha11 <ibkaanhar1@gmail.com>
2025-07-04 10:56:02 +0700

    feat(login): show available accounts for demo

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -5,6 +5,7 @@ import { Head, useForm } from '@inertiajs/vue3';
 import { LoaderCircle } from 'lucide-vue-next';
 import AuthInputWithIcon from '@/components/auth/AuthInputWithIcon.vue';
 import { ref } from 'vue';
+import InfoIcon from '@/components/icons/InfoIcon.vue';
 
 defineProps<{
     status?: string;
@@ -30,6 +31,13 @@ const submit = () => {
     });
 };
 
+const submitWithThisRole = (role: string) => {
+    form.username = role;
+    form.password = 'password';
+
+    submit();
+};
+
 const togglePasswordVisibility = () => {
     showPassword.value = !showPassword.value;
 };
@@ -37,7 +45,9 @@ const togglePasswordVisibility = () => {
 
 <template>
     <Head title="Log in" />
-    <div class="min-h-screen flex flex-col items-center justify-center  p-4">
+    <div class="min-h-screen flex items-center flex-col md:flex-row justify-center gap-4  p-4">
+        <div class="w-full max-w-[320px] rounded-[10px] p-[28px_16px]">
+        </div>
         <div class="w-full max-w-[400px] bg-card rounded-[10px] p-[28px_16px] shadow-xl flex flex-col items-center">
             <h1 class="text-[25px] font-bold font-[Epilogue] text-card-foreground text-center mb-[18px] tracking-(--tracking-title)">
                 Login
@@ -94,6 +104,78 @@ const togglePasswordVisibility = () => {
                     Masuk
                 </Button>
             </form>
+        </div>
+        <div class="w-full max-w-[320px] bg-card rounded-[10px] p-[28px_16px] shadow-xl flex flex-col items-center">
+            <div class="w-full flex items-center mb-2">
+                <InfoIcon class="w-6 h-6 mb-1 mr-2 text-amber-400" />
+                <h1 class="text-[25px] font-bold font-[Epilogue] text-card-foreground tracking-(--tracking-title)">
+                    Demo Accounts
+                </h1>
+            </div>
+            <div class="w-full mb-2">
+                <div class="w-full flex items-center mb-2">
+                    <h2 class="text-[13px] font-bold font-[Inter] text-card-foreground tracking-(--tracking-body)">
+                        Admin Role
+                    </h2>
+                </div>
+                <div class="w-full bg-amber-50 border border-amber-100 rounded-lg p-2">
+                    <p class="text-xs text-gray-700 font-['Inter'] line-clamp-2 leading-relaxed">
+                        Username: <span class="font-bold">admin</span>
+                    </p>
+                    <p class="text-xs text-gray-700 font-['Inter'] line-clamp-2 leading-relaxed">
+                        Password: <span class="font-bold">password</span>
+                    </p>
+                    <button 
+                        class="w-full bg-primary mt-2 text-white text-xs font-bold font-['Inter'] rounded-lg py-2 px-3 text-center hover:from-blue-600 hover:to-indigo-700 cursor-pointer transition-all duration-300 transform shadow-md"
+                        @click="submitWithThisRole('admin')"
+                    >
+                        Use Account
+                    </button>
+
+                </div>
+            </div>
+            <div class="w-full mb-2">
+                <div class="w-full flex items-center mb-2">
+                    <h2 class="text-[13px] font-bold font-[Inter] text-card-foreground tracking-(--tracking-body)">
+                        Apoteker Role
+                    </h2>
+                </div>
+                <div class="w-full bg-amber-50 border border-amber-100 rounded-lg p-2">
+                    <p class="text-xs text-gray-700 font-['Inter'] line-clamp-2 leading-relaxed">
+                        Username: <span class="font-bold">apoteker</span>
+                    </p>
+                    <p class="text-xs text-gray-700 font-['Inter'] line-clamp-2 leading-relaxed">
+                        Password: <span class="font-bold">password</span>
+                    </p>
+                    <button 
+                        class="w-full bg-primary mt-2 text-white text-xs font-bold font-['Inter'] rounded-lg py-2 px-3 text-center hover:from-blue-600 hover:to-indigo-700 cursor-pointer transition-all duration-300 transform shadow-md"
+                        @click="submitWithThisRole('apoteker')"
+                    >
+                        Use Account
+                    </button>
+                </div>
+            </div>
+            <div class="w-full mb-2">
+                <div class="w-full flex items-center mb-2">
+                    <h2 class="text-[13px] font-bold font-[Inter] text-card-foreground tracking-(--tracking-body)">
+                        Dokter Role
+                    </h2>
+                </div>
+                <div class="w-full bg-amber-50 border border-amber-100 rounded-lg p-2">
+                    <p class="text-xs text-gray-700 font-['Inter'] line-clamp-2 leading-relaxed">
+                        Username: <span class="font-bold">doctor</span>
+                    </p>
+                    <p class="text-xs text-gray-700 font-['Inter'] line-clamp-2 leading-relaxed">
+                        Password: <span class="font-bold">password</span>
+                    </p>
+                    <button 
+                        class="w-full bg-primary mt-2 text-white text-xs font-bold font-['Inter'] rounded-lg py-2 px-3 text-center hover:from-blue-600 hover:to-indigo-700 cursor-pointer transition-all duration-300 transform shadow-md"
+                        @click="submitWithThisRole('doctor')"
+                    >
+                        Use Account
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
